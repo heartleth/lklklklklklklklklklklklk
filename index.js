fa48ca3e8275d8865051e4e19138d238d8a19d67
heartleth <myjmyjmyj237@gmail.com>
2023-01-29 03:18:04 +0900

    pl2

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.whenReady().then(() => {
             res.send(JSON.stringify({ builtComponents, actions, states }));
         });
         expressApp.get('/', (req, res) => {
-            res.send(html + '<script src="/payload.js"></script><link rel="stylesheet" href="default.css">');
+            res.send('<meta name="viewport" content="width=device-width, initial-scale=1.0">' + html + '<script src="/payload.js"></script><link rel="stylesheet" href="default.css">');
         });
         expressApp.listen(port);
         shell.openExternal(`http://localhost:${port}/`);
