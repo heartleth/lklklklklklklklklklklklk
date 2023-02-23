93e1e0901fa46eb1d21601cc5939723ed071a40b
heartleth <myjmyjmyj237@gmail.com>
2023-02-24 04:01:19 +0900

    m

--- view/workspace/ui/titlesmenu.js
+++ view/workspace/ui/titlesmenu.js
@@ -3,6 +3,8 @@ function titlesmenu(ws, edt) {
     if (edt) {
         let t = new ElementComponent('h' + edt.tagName[1], [
             [':onload', '@#b' + tok + 'OnLoad/functor'],
+            [':id', '@#b' + tok + 'Id/val'],
+            [':class', '@#b' + tok + 'Class/val'],
             ['innerHTML', '@#b' + tok + 'Text/val']
         ]);
         let uiedit = make('ui-edit').elem;
@@ -18,6 +20,8 @@ function titlesmenu(ws, edt) {
     }
     let t = new ElementComponent('@#titleLevel/value', [
         [':onload', '@#b' + tok + 'OnLoad/functor'],
+        [':id', '@#b' + tok + 'Id/val'],
+        [':class', '@#b' + tok + 'Class/val'],
         ['innerHTML', '@#b' + tok + 'Text/val']
     ]);
     let uiedit = make('ui-edit').elem;
@@ -53,7 +57,7 @@ function paragraphMenu(ws, edt) {
         ]);
         let ac = addc(uiedit.addc(), [div], edt);
         uiedit.then = ac;
-        addChilds(ws, [wstitle('Box'), ac, uiedit]);
+        addChilds(ws, [wstitle('Paragraph'), ac, uiedit]);
         ac.updateref();
     }
     else {
@@ -75,7 +79,7 @@ function paragraphMenu(ws, edt) {
         ]);
         let ac = addc(uiedit.addc(), [div]);
         uiedit.then = ac;
-        addChilds(ws, [wstitle('Box'), ac, uiedit]);
+        addChilds(ws, [wstitle('Paragraph'), ac, uiedit]);
         ac.updateref();
     }
 }
@@ -84,8 +88,25 @@ function formMenu(ws, edt) {
     let tok = Math.floor(1000 * Math.random());
     if (edt) {
         
-        return;
     }
+    let t = new ElementComponent('@#titleLevel/value', [
+        [':id', '@#b' + tok + 'Id/functor'],
+        [':Class', '@#b' + tok + 'Class/functor'],
+        [':onload', '@#b' + tok + 'OnLoad/functor'],
+        ['', '@#b' + tok + 'Text/val']
+    ]);
+    let uiedit = make('ui-edit').elem;
+    uiedit.name = 'b' + tok;
+    uiedit.content = elementPropertySet().concat([
+        make('value-input').set('fname', ['Text', 0]).set('defaultText', '<li>item</li>').elem,
+        make('select').set('fname', ['Type', 1]).opts(['password', 'number', 'date', 'email']).elem,
+        make('value-input').set('fname', ['W', 1, 'Width']).set('defaultText', '5').elem,
+        make('value-input').set('fname', ['H', 1, 'Height']).set('defaultText', '3').elem
+    ]);
+    let ac = addc(uiedit.addc(), [t]);
+    uiedit.then = ac;
+    addChilds(ws, [wstitle('List'), ac, uiedit]);
+    ac.updateref();
 }
 
 function imageMenu(ws, edt) {
@@ -99,9 +120,35 @@ function imageMenu(ws, edt) {
 function listMenu(ws, edt) {
     let tok = Math.floor(1000 * Math.random());
     if (edt) {
-        
+        let t = new ElementComponent(edt.tagName, [
+            [':onload', '@#b' + tok + 'OnLoad/functor'],
+            ['innerHTML', '@#b' + tok + 'Text/val']
+        ]);
+        let uiedit = make('ui-edit').elem;
+        uiedit.name = 'b' + tok;
+        uiedit.content = elementPropertySet().concat([
+            make('value-input').set('fname', ['Text', 0]).set('defaultText', edt.innerText).elem,
+        ]);
+        let ac = addc(uiedit.addc(), [t], edt);
+        uiedit.then = ac;
+        addChilds(ws, [wstitle('List'), ac, uiedit]);
+        ac.updateref();
         return;
     }
+    let t = new ElementComponent('@#titleLevel/value', [
+        [':onload', '@#b' + tok + 'OnLoad/functor'],
+        ['innerHTML', '@#b' + tok + 'Text/val']
+    ]);
+    let uiedit = make('ui-edit').elem;
+    uiedit.name = 'b' + tok;
+    uiedit.content = elementPropertySet().concat([
+        make('value-input').set('fname', ['Text', 0]).set('defaultText', '<li>item</li>').elem,
+        make('select').set('fname', ['Level', 1, 'List type']).opts(['ol', 'ul']).elem,
+    ]);
+    let ac = addc(uiedit.addc(), [t]);
+    uiedit.then = ac;
+    addChilds(ws, [wstitle('List'), ac, uiedit]);
+    ac.updateref();
 }
 
 function iframeMenu(ws, edt) {
